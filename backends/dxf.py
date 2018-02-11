28d19672a867bd87192a27763f884b28681c5824
Alexandre Devert <alex_devert@adagene.cn>
2018-02-11 09:15:27 +0100

    Initial commit

---
+++ backends/dxf.py
@@ -0,0 +1,66 @@
+def write(out, geom):
+	# Generate the header
+	out.write(' 999\n')
+	out.write('DXF created from shape-to-dxf\n')
+	out.write(' 0\n')
+	out.write('SECTION\n')
+	out.write(' 2\n')
+	out.write('HEADER\n')
+	out.write(' 9\n')
+	out.write('$ACADVER\n')
+	out.write(' 1\n')
+	out.write('AC1009\n')
+	out.write(' 0\n')
+	out.write('ENDSEC\n')
+
+	out.write(' 0\n')
+	out.write('SECTION\n')
+	out.write(' 2\n')
+	out.write('TABLES\n')
+	out.write(' 0\n')
+	out.write('TABLE\n')
+	out.write(' 2\n')
+	out.write('LAYER\n')
+	out.write(' 0\n')
+	out.write('LAYER\n')
+	out.write('2\n')
+	out.write('1\n')
+	out.write('0\n')
+	out.write('ENDTAB\n')
+	out.write(' 0\n')
+	out.write('ENDSEC\n')
+	out.write(' 0\n')
+	out.write('SECTION\n')
+	out.write(' 2\n')
+	out.write('BLOCKS\n')
+	out.write(' 0\n')
+	out.write('ENDSEC\n')
+
+	# For each ring
+	ring_list = [geom.exterior] + list(geom.interiors)
+	out.write(' 0\n')
+	out.write('SECTION\n')
+	out.write(' 2\n')
+	out.write('ENTITIES\n')
+
+	for ring in ring_list:
+		coords = ring.coords
+		for vertex_pair in zip(ring.coords[:-1], ring.coords[1:]):
+			out.write(' 0\n')
+			out.write('LINE\n')
+			out.write(' 8\n')
+			out.write('1\n')
+			out.write(' 62\n')
+			out.write('1\n')
+			for i, (x, y) in enumerate(vertex_pair):
+				out.write(' %d\n' % (10 + i))
+				out.write('%f\n' % x)
+				out.write(' %d\n' % (20 + i))
+				out.write('%f\n' % y)
+
+	out.write(' 0\n')
+	out.write('ENDSEC\n')
+
+	# Generate the footer
+	out.write(' 0\n')
+	out.write('EOF\n')	
---
+++ gear.py
@@ -0,0 +1,107 @@
+import numpy
+import argparse
+import itertools
+import backends.dxf
+
+from shapely.ops import cascaded_union
+from shapely.geometry import Point, MultiPoint, Polygon, box
+from shapely.affinity import rotate, scale, translate
+
+
+
+def rot_matrix(x):
+	c, s = numpy.cos(x), numpy.sin(x)
+	return numpy.array([[c, -s], [s, c]])
+
+
+
+def rotation(X, angle, center = None):
+	if center is None:
+		return numpy.dot(X, rot_matrix(angle))
+	else:
+		return numpy.dot(X - center, rot_matrix(angle)) + center
+
+
+
+def deg2rad(x):
+	return (numpy.pi / 180) * x
+
+
+
+def generate(tooth_count = 8,
+             tooth_width = 1.,
+             pressure_angle = deg2rad(20.),
+             backlash = 0.,
+             frame_count = 16):
+	tooth_width -= backlash
+	pitch_circumference = tooth_width * 2 * tooth_count
+	pitch_radius = pitch_circumference / (2 * numpy.pi)
+	addendum = tooth_width * (2 / numpy.pi)
+	dedendum = addendum
+	outer_radius = pitch_radius + addendum
+	print pitch_radius - dedendum
+	# Tooth profile
+	profile = numpy.array([
+  	[-(.5 * tooth_width + addendum * numpy.tan(pressure_angle)),  addendum],
+  	[-(.5 * tooth_width - dedendum * numpy.tan(pressure_angle)), -dedendum],
+  	[ (.5 * tooth_width - dedendum * numpy.tan(pressure_angle)), -dedendum],
+  	[ (.5 * tooth_width + addendum * numpy.tan(pressure_angle)) , addendum]
+	])
+
+	outer_circle = Point(0., 0.).buffer(outer_radius)
+
+	poly_list = []
+	prev_X = None
+	l = 2 * tooth_width / pitch_radius
+	for theta in numpy.linspace(0, l, frame_count):
+		X = rotation(profile + numpy.array((-theta * pitch_radius, pitch_radius)), theta)
+		if prev_X is not None:
+			poly_list.append(MultiPoint([x for x in X] + [x for x in prev_X]).convex_hull)
+		prev_X = X	
+
+	def circle_sector(angle, r):
+		box_a = rotate(box(0., -2 * r, 2 * r, 2 * r), -angle / 2, Point(0., 0.))
+		box_b = rotate(box(-2 * r, -2 * r, 0, 2 * r),  angle / 2, Point(0., 0.))
+		return Point(0., 0.).buffer(r).difference(box_a.union(box_b))
+
+	# Generate a tooth profile
+	tooth_poly = cascaded_union(poly_list)
+	tooth_poly = tooth_poly.union(scale(tooth_poly, -1, 1, 1, Point(0., 0.)))
+
+	# Generate the full gear
+	gear_poly = Point(0., 0.).buffer(outer_radius)
+	for i in range(0, tooth_count):
+		gear_poly = rotate(gear_poly.difference(tooth_poly), (2 * numpy.pi) / tooth_count, Point(0., 0.), use_radians = True)
+	
+	# Job done
+	return gear_poly, pitch_radius
+
+
+
+def main():
+	# Command line parsing
+	parser = argparse.ArgumentParser(description = 'Generate 2d spur gears profiles')
+	parser.add_argument('-t', '--tooth-count', type = int, default = 17, help = 'Tooth count')
+	parser.add_argument('-w', '--tooth-width', type = float, default = 10., help = 'Tooth width')
+	parser.add_argument('-p', '--pressure-angle', type = float, default = 20., help = 'Pressure angle in degrees')
+	parser.add_argument('-n', '--frame-count', type = int, default = 16, help = 'Number of frames used to build the involute')
+	parser.add_argument('-b', '--backlash', type = float, default = 0.2, help = 'Backlash')
+	parser.add_argument('-o', '--output-path', default = 'out.dxf', help = 'Output file')
+	args = parser.parse_args()
+
+	# Generate the shape
+	poly, pitch_radius = generate(args.tooth_count,
+	                              args.tooth_width,
+	                              deg2rad(args.pressure_angle),
+	                              args.backlash,
+	                              args.frame_count)
+
+	# Save the shape
+	print 'pitch radius =', pitch_radius
+	with open(args.output_path, 'w') as f:
+		backends.dxf.write(f, poly)
+
+
+
+if __name__ == '__main__':
+	main()
